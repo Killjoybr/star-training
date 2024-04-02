aaf83338851aed415f9ea86d68b37d2abf53f7dd
killjoybr <112730142+Killjoybr@users.noreply.github.com>
2024-04-02 13:34:49 -0300

    adicionando super classe

---
+++ participante.java
@@ -0,0 +1,36 @@
+public class participante {
+    public String nome;
+    public int idade;
+    public String cpf;
+
+    public participante(String nome, int idade, String cpf) {
+        this.nome = nome;
+        this.idade = idade;
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+    
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+}
